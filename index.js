5888770e531ec6c30f9613515d9a3c678faf22d0
sajjadhossain <sajjadhossain0756@gmail.com>
2024-09-11 20:26:52 +0600

    problem-4 complete

--- index.js
+++ index.js
@@ -15,20 +15,66 @@
 
 // problems-2 start here
 
-function sendNotification(email) {
+// function sendNotification(email) {
     
-    if(email.indexOf("@") < 1){
-         return "Invalid Email";
-    }   
+//     if(email.indexOf("@") < 1){
+//          return "Invalid Email";
+//     }   
 
-    let name = email.split("@");
-    let userName = name[0];
-    let domainName = name[1];
-    let notification = `${userName} sent you an email from ${domainName}`;
+//     let name = email.split("@");
+//     let userName = name[0];
+//     let domainName = name[1];
+//     let notification = `${userName} sent you an email from ${domainName}`;
     
-    return notification;    
+//     return notification;    
     
+// }
+
+// const getNotification = sendNotification("sadia8icloud.com");
+// console.log(getNotification);
+
+// problem-3 start here
+
+// function checkDigitsInName(name) {
+//     const sajjad = name.split("");
+//     console.log(sajjad.lenght);
+//     for(let i =0; i < sajjad.lenght; i++){
+        
+//         if( i <= 0 || item > 0){
+//             return true;
+//         }
+//         else{
+//             return false
+//         }
+//     }
+    
+// }
+
+// const result = checkDigitsInName("rahim0");
+// console.log(result);
+
+// problem-4 start here
+
+function calculateFinalScore(obj) {
+
+     if(typeof obj !== "object"){
+        return "Invalid Input";
+     }
+
+     let totalScore = obj.testScore + obj.schoolGrade;
+
+     if(obj.isFFamily === true){
+        totalScore += 20;
+     }
+     
+     const finalScore = totalScore >= 80 ? true : false ;
+
+     return finalScore;
+     
 }
 
-const getNotification = sendNotification("sadia8icloud.com");
-console.log(getNotification);
+const person1 = { name: "Rajib", testScore: 45,  schoolGrade: 35, isFFamily : false };
+
+const resultCalculate = calculateFinalScore(person1); 
+
+console.log(resultCalculate);
